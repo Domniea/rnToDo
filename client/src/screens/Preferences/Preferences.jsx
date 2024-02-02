4f12965431757e3c20f3f4fde76d2df7c366cba6
domniea <domniea@gmail.com>
2024-02-02 11:08:43 -0700

    Delete user UI

--- client/src/screens/Preferences/Preferences.jsx
+++ client/src/screens/Preferences/Preferences.jsx
@@ -1,30 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useState } from 'react'
+import { useState, useContext} from 'react'
+import { deleteUser } from 'aws-amplify/auth';
+import { UserContext } from '../../context/UserProvider';
 
 import CustomButton from '../../components/CustomButton'
 
 const Preferences = () => {
 
-const [toggleDeleteWarn, setToggleDeleteWarn] = useState(false)
+    const {
+        setUser
+    } = useContext(UserContext)
 
+    const [toggleDeleteWarn, setToggleDeleteWarn] = useState(false)
 
-function toggleDeleteWarning() {
-    setToggleDeleteWarn(prevState => !prevState)
-}
+    function toggleDeleteWarning() {
+        setToggleDeleteWarn(prevState => !prevState)
+        setTimeout(() => {
+            console.log("first")
+            }, 5000
+        );
+        setTimeout(() => {
+            console.log("second")
+            }, 2000
+        );
+
+    }
+
+    async function handleDelete() {
+        try {
+            // await deleteUser()
+            console.log('Your account has been permanently deleted')
+            // setTimeout(() => {
+            //     setUser(undefined)
+            //     }, 5000
+            // )
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
 
   return (
     <View style={styles.container}>
         <Text style={styles.header}>Preferences</Text>
         { 
-            toggleDeleteWarn &&
+            !toggleDeleteWarn ?
+            <>
+              <CustomButton text='Delete Account' onPress={toggleDeleteWarning}/>
+            </>
+            :
             <>
                 <View>
                     <Text>Are you sure you want to delete your account?</Text>
+                    <CustomButton text ='Delete Account' />
+                    <CustomButton text='Go Back' onPress={toggleDeleteWarning}/>
                 </View>
             </>     
         }
-        <CustomButton text='Delete Account' onPress={toggleDeleteWarning}/>
+        
     </View>
   )
 }
